4fe3298c43823f618bca589cb09ddf3a67aa3021
Martyn Green <martyn.robert.green@gmail.com>
2020-03-01 19:16:36 +0000

    Created initital release compatible with 1.1

---
+++ Source/InvisiblePowerLines/InvisiblePowerLines.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+[StaticConstructorOnStartup]
+internal class SmallWireGoAway
+{
+    static SmallWireGoAway()
+    {
+        var harmony = new Harmony("OdysseyScorpio.InvisibleConduit.202003");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ Source/InvisiblePowerLines/Patch_InvisiblePowerLines.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace GoAwaySmallWire
+{
+    [HarmonyPatch(typeof(PowerNetGraphics))]
+    [HarmonyPatch("PrintWirePieceConnecting")]
+    internal class Patch_ThatSmallWire
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
